0df1f8af659c425516eca96fe716b153bf194dbd
eldorado-22 <jumashevv992@gmail.com>
2023-06-07 17:43:20 +0600

    hakaton-dispatch

---
+++ src/components/DispatchServices/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./style.scss";
+
+const DispatchServices = () => {
+    return (
+        <section id="dispatch">
+            <div className="container">
+                <div className="dispatch">
+
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default DispatchServices;
---
+++ src/components/Header/header.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./style.scss";
+
+const Header = () => {
+    return (
+        <header id="header">
+            <div className="container">
+                <div className="header">
+
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
