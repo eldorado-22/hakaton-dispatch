206fc597c858dcaa21e7e4f2a0d881aa1bcb78f9
Eldorado.JM <119120263+eldorado-22@users.noreply.github.com>
2023-06-07 15:25:31 +0300

    header (#1)

--- src/components/Hero/hero.tsx
+++ src/components/Hero/hero.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import "./hero.scss";
+import tech from "../../images/tachka 1.svg"
 
 const Hero = () => {
     return (
         <section id="hero">
             <div className="container">
                 <div className="hero">
-
+                    <img src={tech} alt=""/>
                 </div>
             </div>
         </section>
